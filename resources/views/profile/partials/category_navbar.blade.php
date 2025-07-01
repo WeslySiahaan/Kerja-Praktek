88d25e4508cb227c90c58dfd98442cd5c4fff5b3
Sunanda E N Munthe <149130122+nndamnth@users.noreply.github.com>
2025-07-01 14:41:48 +0700

    Kategori

--- resources/views/profile/partials/category_navbar.blade.php
+++ resources/views/profile/partials/category_navbar.blade.php
@@ -4,70 +4,68 @@
     <div class="container-fluid mt-2">
         <div class="d-flex align-items-center">
             <div class="category-buttons">
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="all">All</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Sudden Wealth">Sudden Wealth</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Werewolves">Werewolves</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Popular">Popular</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Avenge">Avenge</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Divine Tycoon">Divine Tycoon</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Love Triangle">Love Triangle</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Revenge">Revenge</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="all">Semua</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Kekayaan Mendadak">Kekayaan Mendadak</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Manusia Serigala">Manusia Serigala</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Populer">Populer</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Balas Dendam">Balas Dendam</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Konglomerat Ilahi">Konglomerat Ilahi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta Segitiga">Cinta Segitiga</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Paranormal">Paranormal</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Marriage">Marriage</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Pernikahan">Pernikahan</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinderella">Cinderella</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Underdog Rise">Underdog Rise</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Son-in-Law">Son-in-Law</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Secret Identity">Secret Identity</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Second-chance Love">Second-chance Love</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Comedy">Comedy</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Boy's Love">Boy's Love</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Marriage Before Love">Marriage Before Love</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Bangkit dari Keterpurukan">Bangkit dari Keterpurukan</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Menantu Lelaki">Menantu Lelaki</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Identitas Rahasia">Identitas Rahasia</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta Kesempatan Kedua">Cinta Kesempatan Kedua</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Komedi">Komedi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta Sesama Laki-laki">Cinta Sesama Laki-laki</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Menikah Sebelum Cinta">Menikah Sebelum Cinta</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Mafia">Mafia</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Influencer">Influencer</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Forbidden Love">Forbidden Love</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Uplifting Series">Uplifting Series</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Strong Female Lead">Strong Female Lead</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Romance">Romance</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta Terlarang">Cinta Terlarang</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cerita Inspiratif">Cerita Inspiratif</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Tokoh Perempuan Kuat">Tokoh Perempuan Kuat</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Romansa">Romansa</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="CEO">CEO</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Harem">Harem</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Fantasy">Fantasy</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Information Gaps">Information Gaps</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Soulmate">Soulmate</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Trending">Trending</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Concealed Identity">Concealed Identity</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Counterattack">Counterattack</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Disguise">Disguise</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Sweet Love">Sweet Love</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Suspense">Suspense</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Betrayal">Betrayal</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Urban">Urban</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cross-dressing">Cross-dressing</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Time Travel Harem">Time Travel Harem</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Werewolf">Werewolf</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="SM">SM</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Enemies to Lovers">Enemies to Lovers</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Mystery">Mystery</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Super Power">Super Power</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Billionaire">Billionaire</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Hated">Hated</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Dominant">Dominant</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Alternative History">Alternative History</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Badboy">Badboy</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Rebirth">Rebirth</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Small Potato">Small Potato</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Contract Lover">Contract Lover</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Wealthy">Wealthy</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Fantasi">Fantasi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Kesenjangan Informasi">Kesenjangan Informasi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Belahan Jiwa">Belahan Jiwa</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Sedang Tren">Sedang Tren</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Identitas Tersembunyi">Identitas Tersembunyi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Serangan Balik">Serangan Balik</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Penyamaran">Penyamaran</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta Manis">Cinta Manis</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Ketegangan">Ketegangan</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Pengkhianatan">Pengkhianatan</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Kehidupan Urban">Kehidupan Urban</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Penyamaran Gender">Penyamaran Gender</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Harem Penjelajah Waktu">Harem Penjelajah Waktu</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Dominasi & Submisi">Dominasi & Submisi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Dari Musuh Jadi Cinta">Dari Musuh Jadi Cinta</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Misteri">Misteri</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Kekuatan Super">Kekuatan Super</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Miliarder">Miliarder</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Dibenci">Dibenci</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Dominan">Dominan</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Sejarah Alternatif">Sejarah Alternatif</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Anak Nakal">Anak Nakal</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Reinkarnasi">Reinkarnasi</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Si Kecil yang Diremehkan">Si Kecil yang Diremehkan</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Pasangan Kontrak">Pasangan Kontrak</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Keluarga Kaya">Keluarga Kaya</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Humor">Humor</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Misunderstanding">Misunderstanding</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="True Love">True Love</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Kesalahpahaman">Kesalahpahaman</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta Sejati">Cinta Sejati</button>
                 <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Comeback">Comeback</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Toxic Relationship">Toxic Relationship</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Contract Marriage">Contract Marriage</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Family">Family</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Time Travel">Time Travel</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Bitter Love">Bitter Love</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Steamy">Steamy</button>
-                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Destiny">Destiny</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Hubungan Beracun">Hubungan Beracun</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Pernikahan Kontrak">Pernikahan Kontrak</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Keluarga">Keluarga</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Perjalanan Waktu">Perjalanan Waktu</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cinta yang Menyakitkan">Cinta yang Menyakitkan</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Cerita Panas">Cerita Panas</button>
+                <button class="btn btn-sm btn-outline-secondary me-2 mb-2" data-category="Takdir">Takdir</button>
             </div>
         </div>
     </div>
